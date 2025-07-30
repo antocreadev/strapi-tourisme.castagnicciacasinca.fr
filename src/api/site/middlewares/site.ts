c45047228b75fc22905fe2db3cc5b5b6de05fea4
antocreadev <anthonymenghi@icloud.com>
2025-07-30 08:33:28 +0000

    feat: ajout de la logique de peuplement dans le middleware et configuration des routes pour le site

--- src/api/site/middlewares/site.ts
+++ src/api/site/middlewares/site.ts
@@ -3,10 +3,16 @@
  */
 
 import type { Core } from '@strapi/strapi';
-
+const populate = [
+  "commune",
+  "Liens",
+  "Liens.image",
+  "Images"
+];
 export default (config, { strapi }: { strapi: Core.Strapi }) => {
   // Add your own logic here.
   return async (ctx, next) => {
+    ctx.query.populate = populate;
     strapi.log.info('In site middleware.');
 
     await next();
--- src/api/site/routes/site.ts
+++ src/api/site/routes/site.ts
@@ -4,4 +4,10 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::site.site');
+export default factories.createCoreRouter('api::site.site', {
+  config: {
+    find: {
+      middlewares: ["api::site.site"],
+    },
+  },
+});
